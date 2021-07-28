247899010f20f15a26651fe0cb91366bb72445a4
sebademasi10 <sebademasi10@gmail.com>
2021-07-28 09:16:21 -0300

    5 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,24 +8,28 @@ import { Component, VERSION } from '@angular/core';
 export class AppComponent {
   name = 'Angular ' + VERSION.major;
   nEsPrimo: boolean;
+  estaEvaluado = false;
 
   esPrimo(n: number): boolean {
     if (n !== 2 && (this.esPar(n) || n.toString()[-1] == '5')) {
+      this.estaEvaluado = true;
       return (this.nEsPrimo = false);
     }
 
     let raiz = Math.sqrt(n);
 
     if (this.esEntero(raiz)) {
+      this.estaEvaluado = true;
       return (this.nEsPrimo = false);
     } else {
       for (let i = 3; i < Math.round(raiz); i += 2) {
         if (n % i == 0) {
+          this.estaEvaluado = true;
           return (this.nEsPrimo = false);
         }
       }
     }
-
+    this.estaEvaluado = true;
     return (this.nEsPrimo = true);
   }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,23 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
+    MatIconModule
+  ],
+  declarations: [AppComponent, HelloComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
